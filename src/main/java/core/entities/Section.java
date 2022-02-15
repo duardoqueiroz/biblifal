075ed65c4d426c9dbbd632da76acefa2fa3a39b5
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-15 17:45:21 -0300

    feat: section class

---
+++ src/main/java/core/entities/Section.java
@@ -0,0 +1,32 @@
+package core.entities;
+
+public class Section {
+  private String name;
+  private String description;
+
+  public Section(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "Section [description=" + description + ", name=" + name + "]";
+  }
+}
