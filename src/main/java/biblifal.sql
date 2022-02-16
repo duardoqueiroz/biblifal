cfeb3124c97359ffedaf3ce6515084fc842419e2
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-15 23:23:29 -0300

    chore: modify sql

--- src/main/java/biblifal.sql
+++ src/main/java/biblifal.sql
@@ -2,34 +2,55 @@ create database biblifal;
 
 \connect biblifal;
 
-CREATE TABLE user(
+CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   email VARCHAR(150) NOT NULL,
   is_manager BOOLEAN NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE section(
-  id SERIAL,
-  name VARCHAR(100) NOT NULL PRIMARY KEY,
+CREATE TABLE sections(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
   description TEXT
 );
 
-CREATE TABLE shelf(
+CREATE TABLE shelfs(
   id SERIAL PRIMARY KEY,
   section_id INT NOT NULL,
   capacity INT NOT NULL,
 
-  CONSTRAINT section_fk FOREIGN KEY (section_id) REFERENCES section (id)
+  CONSTRAINT section_fk FOREIGN KEY (section_id) REFERENCES sections (id)
 );
 
-CREATE TABLE book(
-  id SERIAL,
+CREATE TABLE books(
+  id SERIAL PRIMARY KEY,
   title VARCHAR(100) NOT NULL,
   publishing_company VARCHAR(50),
   author VARCHAR NOT NULL,
-  price NUMERIC(6,2) NOT NULL,
-  shelf_id INT NOT NULL,
+  price NUMERIC(6,2) NOT NULL
+);
 
-  CONSTRAINT shelf_fk FOREIGN KEY (shelf_id) REFERENCES shelf (id)
+CREATE TABLE genres(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+CREATE TABLE book_genres(
+  id SERIAL PRIMARY KEY,
+  book_id INT,
+  genre_id INT,
+
+  CONSTRAINT book_fk FOREIGN KEY (book_id) REFERENCES books (id),
+  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genres (id)
+);
+
+CREATE TABLE shelf_books(
+  id SERIAL PRIMARY KEY,
+  shelf_id INT,
+  book_id INT,
+  quantity INT,
+
+  CONSTRAINT shelf_fk FOREIGN KEY (shelf_id) REFERENCES shelfs (id),
+  CONSTRAINT book_fk FOREIGN KEY (book_id) REFERENCES books (id)
 );
