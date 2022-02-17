14b5d2f5966e903282eb42856611f98359fd26fa
duardoqueiroz <eduardoldq1@gmail.com>
2022-02-17 14:59:20 -0300

    feat: add password column in users table

--- src/main/java/biblifal.sql
+++ src/main/java/biblifal.sql
@@ -3,9 +3,10 @@ create database biblifal;
 \connect biblifal;
 
 CREATE TABLE users(
-  id SERIAL PRIMARY KEY,
+  id SERIAL,
   name VARCHAR(50) NOT NULL,
-  email VARCHAR(150) NOT NULL,
+  email VARCHAR(150) NOT NULL PRIMARY KEY,
+  password VARCHAR(500) NOT NULL,
   is_manager BOOLEAN NOT NULL DEFAULT FALSE
 );
 
