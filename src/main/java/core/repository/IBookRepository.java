56bbafbba0a18f394b38338f3926177395766d36
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2022-02-22 17:06:33 -0300

    fix: just changing the functions order

--- src/main/java/core/repository/IBookRepository.java
+++ src/main/java/core/repository/IBookRepository.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import core.entities.Book;
 
 public interface IBookRepository {
-  void remove(int bookId);
 
   void save(Book book);
+
+  void remove(int bookId);
 
   ArrayList<Book> findAll();
 
