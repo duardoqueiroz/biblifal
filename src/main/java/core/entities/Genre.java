6325380f69ac5f3c6d38dc082c00e88ee5cfcfa1
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2022-02-24 17:03:40 -0300

    chore: implementing id on genre

--- src/main/java/core/entities/Genre.java
+++ src/main/java/core/entities/Genre.java
@@ -1,10 +1,16 @@
 package core.entities;
 
 public class Genre {
+  private int id;
   private String name;
 
-  public Genre(String name) {
+  public Genre(int id, String name) {
+    this.id = id;
     this.name = name;
+  }
+
+  public int getId() {
+    return id;
   }
 
   public String getName() {
