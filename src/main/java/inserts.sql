344fda4c94d30fb57dff5db31217492b9462a8db
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2022-03-06 15:55:48 -0300

    fix: simple quote (aspas simples) to string on query

--- src/main/java/inserts.sql
+++ src/main/java/inserts.sql
@@ -3,18 +3,18 @@
 TRUNCATE users, sections, shelfs, books, genres, book_genres, shelf_books;
 
 --- users ---
-INSERT INTO users (id, name, email, password, is_manager) VALUES (1, "Guilherme de Jesus", "gui@aluno.ifal.edu.br", "12345678", DEFAULT);
+INSERT INTO users (id, name, email, password, is_manager) VALUES (DEFAULT, 'Guilherme de Jesus', 'gui@aluno.ifal.edu.br', '12345678', DEFAULT);
 
-INSERT INTO users (id, name, email, password, is_manager) VALUES (2, "Eduardo Queiroz", "dudu@aluno.ifal.edu.br", "12345678", DEFAULT);
+INSERT INTO users (id, name, email, password, is_manager) VALUES (DEFAULT, 'Eduardo Queiroz', 'dudu@aluno.ifal.edu.br', '12345678', DEFAULT);
 
-INSERT INTO users (id, name, email, password, is_manager) VALUES (3, "Jesse Diego", "jess@aluno.ifal.edu.br", "12345678", DEFAULT);
+INSERT INTO users (id, name, email, password, is_manager) VALUES (DEFAULT, 'Jesse Diego', 'jess@aluno.ifal.edu.br', '12345678', DEFAULT);
 
 --- sections ---
-INSERT INTO sections (id, name, description) VALUES (1, "Nacional", "Seção de livros nacionais.");
+INSERT INTO sections (id, name, description) VALUES (1, 'Nacional', 'Seção de livros nacionais.');
 
-INSERT INTO sections (id, name, description) VALUES (2, "Infantil", "Seção de livros para crianças.");
+INSERT INTO sections (id, name, description) VALUES (2, 'Infantil', 'Seção de livros para crianças.');
 
-INSERT INTO sections (id, name, description) VALUES (3, "Jovem", "Seção de livros para jovens.");
+INSERT INTO sections (id, name, description) VALUES (3, 'Jovem', 'Seção de livros para jovens.');
 
 --- shelfs ---
 INSERT INTO shelfs (id, section_id, capacity) VALUES (1, 1, 10);
@@ -23,24 +23,24 @@ INSERT INTO shelfs (id, section_id, capacity) VALUES (3, 3, 10);
 INSERT INTO shelfs (id, section_id, capacity) VALUES (4, 3, 10);
 
 --- books ---
-INSERT INTO books (id, title, publishing_company, author, price ) VALUES (1, "Os Sertões", "Princips", "Euclides da Cunha", 16.99 );
+INSERT INTO books (id, title, publishing_company, author, price ) VALUES (1, 'Os Sertões', 'Princips', 'Euclides da Cunha', 16.99 );
 
-INSERT INTO books (id, title, publishing_company, author, price ) VALUES (2, "Diário de Um Banana", "V&R", "Jeff Kinney", 24.90 );
+INSERT INTO books (id, title, publishing_company, author, price ) VALUES (2, 'Diário de Um Banana', 'V&R', 'Jeff Kinney', 24.90 );
 
-INSERT INTO books (id, title, publishing_company, author, price ) VALUES (3, "Jogos Vorazes", "Rocco", "Suzanne Collins", 34.80);
+INSERT INTO books (id, title, publishing_company, author, price ) VALUES (3, 'Jogos Vorazes', 'Rocco', 'Suzanne Collins', 34.80);
 
-INSERT INTO books (id, title, publishing_company, author, price ) VALUES (4, "A Seleção", "Seguinte", "Kiera Cass", 19.90);
+INSERT INTO books (id, title, publishing_company, author, price ) VALUES (4, 'A Seleção', 'Seguinte', 'Kiera Cass', 19.90);
 
 --- genres ---
-INSERT INTO genres (id, name) VALUES (1, "Livro-Reportagem");
+INSERT INTO genres (id, name) VALUES (1, 'Livro-Reportagem');
 
-INSERT INTO genres (id, name) VALUES (2, "Romance");
+INSERT INTO genres (id, name) VALUES (2, 'Romance');
 
-INSERT INTO genres (id, name) VALUES (3, "Comédia");
+INSERT INTO genres (id, name) VALUES (3, 'Comédia');
 
-INSERT INTO genres (id, name) VALUES (4, "Ação");
+INSERT INTO genres (id, name) VALUES (4, 'Ação');
 
-INSERT INTO genres (id, name) VALUES (5, "Ficção Distópica");
+INSERT INTO genres (id, name) VALUES (5, 'Ficção Distópica');
 
 --- book_genres ---
 
